793169a3befe08148316214b83cab62ffb61f257
Eduardo <eaculed@gmail.com>
2014-12-08 11:33:19 +0000

    Empezando con php

---
+++ busqueda.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">  
+    <title>My Patente</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/queries.css">
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	  <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Sintony:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+		<header class="clearfix">
+		    <div class="logo col-md-3"><h2 class="logo-text">My Patente</h2></div>
+		    <nav class="clearfix">
+            <ul class="clearfix">
+                <li><a href="#patente" class="active">Buscador</a></li>
+                <li><a href="#equipo">Equipo</a></li>
+                <li><a href="./busqueda.php">Búsquedas</a></li>
+                <li><a href="#">how we help</a></li>
+                <li><a href="#">take the tour</a></li>
+                <li><a href="#" class="last">contact</a></li>
+            </ul>
+        </nav>
+        <div class="pullcontainer" id="patente">
+        <a href="#" id="pull"><i class="fa fa-bars fa-2x"></i></a>
+        </div>     
+		</header>
+    <div class="banner">
+    <ul>
+        <li style="background-image: url('img/planos1.jpg');">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6 col-md-offset-3">
+                <div class="hero-title">
+                  Buscador de Patentes
+                </div>
+				<br>
+				<br>
+            </div>
+        </div>
+    </div>
+    </li>
+    </ul>
+    </div>
+</body>
+</html>
